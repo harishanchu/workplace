06c0c8cb98d15394066e6b577e8d0771361b109c
Harish Anchu <harishanchu@gmail.com>
2018-05-18 05:14:45 +0530

    Updates

--- server/components/total-count.js
+++ server/components/total-count.js
@@ -1,30 +1,40 @@
 'use strict';
 
 module.exports = function (app, options) {
-  var remotes = app.remotes();
+  let remotes = app.remotes();
   // Set X-Total-Count for all search requests
-  var applyXTotal = function (ctx, next) {
-    var filter;
+  let applyXTotal = function (ctx, next) {
+    let filter;
     if (ctx.args && ctx.args.filter) {
       filter = ctx.args.filter.where;
     }
 
     if (!ctx.res._headerSent) {
-      if(ctx.method.isStatic) {
-        this.count(filter, function (err, count) {
-          ctx.res.set('X-Total-Count', count);
-          next();
-        });
+      let countCallback = function (err, count) {console.log(arguments)
+        ctx.res.set('X-Total-Count', count);
+        next();
+      };
+
+      if (ctx.method.isStatic) {
+        this.count(filter, countCallback);
       } else {
-        // console.log(ctx.instance)
+        let filter = ctx.req.query.filter || {};
+        let query = {};
+
+        if(filter) {
+          query.filter = filter;
+        }
+
+        ctx.instance[ctx.method.name.replace('__get__', '__count__')](query, countCallback);
       }
     } else {
       next();
     }
   };
-  var pattern = options && Array.isArray(options.pattern) ? options.pattern : ['*.find'];
 
-  for (var i=pattern.length-1; i>=0; i--) {
+  let pattern = options && Array.isArray(options.pattern) ? options.pattern : ['*.find'];
+
+  for (let i = pattern.length - 1; i >= 0; i--) {
     remotes.after(pattern[i], applyXTotal);
   }
 };
