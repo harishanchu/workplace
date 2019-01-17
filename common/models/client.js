82a3fa0f660c38a82d9e5bda25737546f4656141
Harish Anchu <harishanchu@gmail.com>
2019-01-17 21:49:10 +0530

    Updates

--- common/models/client.js
+++ common/models/client.js
@@ -1,5 +1,67 @@
 'use strict';
+const g = require('loopback/lib/globalize');
 
 module.exports = function (Client) {
+  /* ----------------------------
+   * Hooks
+   * ----------------------------
+   */
+  async function beforeDelete(ctx, next) {
+    let ids = [].concat(ctx.args.id);
 
+    let clients = await Client.find({where: {id: {inq: ids}}, fields: {id: true}, include:"projects"});
+
+    if(!clients.every(client => {
+        return !client.projects().length;
+      })) {
+      let err = new Error(g.f('Clients are associated with projects, hence cannot be deleted.'));
+      err.statusCode = 400;
+      err.code = 'CLIENT_ASSOCIATED_WITH_PROJECTS';
+
+      throw err;
+    }
+
+    return null;
+  }
+
+  Client.beforeRemote('deleteById', beforeDelete);
+  Client.beforeRemote('destroyAllCustom', beforeDelete);
+
+  /* -------------------------
+   *  API's
+   * -------------------------
+   */
+  Client.destroyAllCustom = async function (ids, cb) {
+    ids = [].concat(ids);
+
+    return await Client.destroyAll({
+      id: {
+        inq: ids
+      }
+    }, (err, info) => {
+      if (err) {
+        throw err;
+      } else {
+        return info;
+      }
+    })
+  };
+
+  /* ---------------------------------
+   * Remote methods
+   * --------------------------------
+   */
+  Client.remoteMethod('destroyAllCustom', {
+    isStatic: true,
+    description: 'Delete all matching records',
+    accessType: 'WRITE',
+    accepts: {arg: 'id', type: 'array', description: 'id\'s to delete '},
+    returns: {
+      arg: 'count',
+      type: 'object',
+      description: 'The number of instances deleted',
+      root: true,
+    },
+    http: {verb: 'del', path: '/'}
+  });
 };
--- common/models/project.js
+++ common/models/project.js
@@ -1,5 +1,67 @@
 'use strict';
+const g = require('loopback/lib/globalize');
 
 module.exports = function (Project) {
+  /* ----------------------------
+     * Hooks
+     * ----------------------------
+     */
+  async function beforeDelete(ctx, next) {
+    let ids = [].concat(ctx.args.id);
 
+    let projects = await Project.find({where: {id: {inq: ids}}, fields: {id: true}, include:"tasks"});
+
+    if(!projects.every(project => {
+        return !project.tasks().length;
+      })) {
+      let err = new Error(g.f('Projects are associated with tasks, hence cannot be deleted.'));
+      err.statusCode = 400;
+      err.code = 'PROJECT_ASSOCIATED_WITH_TASKS';
+
+      throw err;
+    }
+
+    return null;
+  }
+
+  Project.beforeRemote('deleteById', beforeDelete);
+  Project.beforeRemote('destroyAllCustom', beforeDelete);
+
+  /* -------------------------
+   *  API's
+   * -------------------------
+   */
+  Project.destroyAllCustom = async function (ids, cb) {
+    ids = [].concat(ids);
+
+    return await Project.destroyAll({
+      id: {
+        inq: ids
+      }
+    }, (err, info) => {
+      if (err) {
+        throw err;
+      } else {
+        return info;
+      }
+    })
+  };
+
+  /* ---------------------------------
+   * Remote methods
+   * --------------------------------
+   */
+  Project.remoteMethod('destroyAllCustom', {
+    isStatic: true,
+    description: 'Delete all matching records',
+    accessType: 'WRITE',
+    accepts: {arg: 'id', type: 'array', description: 'id\'s to delete '},
+    returns: {
+      arg: 'count',
+      type: 'object',
+      description: 'The number of instances deleted',
+      root: true,
+    },
+    http: {verb: 'del', path: '/'}
+  });
 };
